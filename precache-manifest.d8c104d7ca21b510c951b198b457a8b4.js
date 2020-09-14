355db8e3e25e8c9edcb2cb8a3d0531dca1ebd055
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-09-14 22:27:10 +0300

    Updates

---
+++ precache-manifest.d8c104d7ca21b510c951b198b457a8b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e86de5c13adbb23a2d9683f54afd4c4",
+    "url": "/Spalsh-API/index.html"
+  },
+  {
+    "revision": "71da168d211704835a5a",
+    "url": "/Spalsh-API/static/css/main.6f840240.chunk.css"
+  },
+  {
+    "revision": "579f3f5333960754c099",
+    "url": "/Spalsh-API/static/js/2.26d07d40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spalsh-API/static/js/2.26d07d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71da168d211704835a5a",
+    "url": "/Spalsh-API/static/js/main.80ec9a50.chunk.js"
+  },
+  {
+    "revision": "7c7af84071720e3c7976",
+    "url": "/Spalsh-API/static/js/runtime-main.6cfff646.js"
+  }
+]);
